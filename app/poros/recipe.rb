e0c2e41f578a061a3f44b57c8144ba11a6c61be1
Kelsie Porter <krporter22@gmail.com>
2023-03-04 21:35:17 -0700

    Create recipe poro and test

---
+++ app/poros/recipe.rb
@@ -0,0 +1,13 @@
+class Recipe 
+  attr_reader :title,
+              :url,
+              :image,
+              :country
+
+  def initialize(recipe_data, country)
+    @title = recipe_data[:recipe][:label]
+    @url = recipe_data[:recipe][:url]
+    @image = recipe_data[:recipe][:image]
+    @country = country
+  end
+end
