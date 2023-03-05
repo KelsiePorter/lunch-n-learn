db275ce598c8ce654e1dd0f6f35154b61c017b11
Kelsie Porter <krporter22@gmail.com>
2023-03-05 11:48:57 -0700

    Complete countries api service structure and assoc tests

--- app/facade/country_facade.rb
+++ app/facade/country_facade.rb
@@ -1,6 +1,6 @@
 class CountryFacade
 
-  def self.get_random_country(countries)
+  def self.get_random_country
     country_results = CountryService.get_countries
     country = Country.new(country_results.sample[:name])
   end
