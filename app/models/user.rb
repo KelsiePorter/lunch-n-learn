0efffc820b287a9c9b7907a44a872a896432b9bf
Kelsie Porter <krporter22@gmail.com>
2023-03-07 16:33:20 -0700

    Add has_many relationship and assoc test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord 
+  has_many :favorites,
+            primary_key: :user_api_key
+
   validates_presence_of :name, :email, :api_key
   validates :email, uniqueness: true
   validates :api_key, uniqueness: true
