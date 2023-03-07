1cb8531d77968eade2da45831bfabae8644151c9
Kelsie Porter <krporter22@gmail.com>
2023-03-07 14:34:08 -0700

    Create user model, validations, gen api and tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User do 
+  describe 'validations' do 
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :api_key }
+
+    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of :api_key }
+  end
+  describe 'create api key' do 
+    it 'generates random 27 character string for users api key' do
+
+      new_user = User.new(name: 'River', email: 'river@example.com')
+
+      expect(new_user.api_key).to be nil
+      
+      new_user.generate_api_key
+
+      expect(new_user.api_key).to be_a String
+    end 
+  end
+end
