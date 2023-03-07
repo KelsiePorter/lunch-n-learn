509569e8414ae959f4b2d044b001913b980d186e
Kelsie Porter <krporter22@gmail.com>
2023-03-06 18:12:03 -0700

    learning resource poro test

--- spec/poros/learning_resource_spec.rb
+++ spec/poros/learning_resource_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe LearningResource do 
   it 'exists and has attributes' do 
-    
+    country = 'peru'
+    video = JSON.parse(File.read('./spec/fixtures/peru_video_data.json'), symbolize_names: true)
+    images = JSON.parse(File.read('./spec/fixtures/peru_images.json'), symbolize_names: true)
+
+    learning_resource = LearningResource.new(country, video, images)
+
+    expect(learning_resource.country).to eq('peru')
+    expect(learning_resource.video).to be_a Hash
+    expect(learning_resource.images).to be_a Hash
   end
 end
