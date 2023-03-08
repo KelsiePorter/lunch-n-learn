107d026e49e671c3c0d16005675539c7e9f0599b
Kelsie Porter <krporter22@gmail.com>
2023-03-07 18:36:41 -0700

    Update relation to go through user_api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord 
-  has_many :favorites,
-            primary_key: :user_api_key
+  has_many :favorites, foreign_key: 'user_api_key', primary_key: 'api_key'
 
   validates_presence_of :name, :email, :api_key
   validates :email, uniqueness: true
